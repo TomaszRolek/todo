4968346079c0dd928155294cd74c4b14150b3360
Tomasz Rolek <tomio7@interia.pl>
2024-11-03 20:31:22 +0100

    delete some unused code

--- lib/services/router/app_router.dart
+++ lib/services/router/app_router.dart
@@ -16,14 +16,6 @@ class AppRouter {
           builder: (BuildContext context, GoRouterState state) {
             return DashboardPage(appRepository: _appRepository);
           }),
-      // GoRoute(
-      //     name: AppRoutes.editGroup,
-      //     path: '/edit_group',
-      //     builder: (BuildContext context, GoRouterState state) {
-      //       final extras = state.extra! as Map<String, dynamic>;
-      //       final groupId = extras['groupId'] as int;
-      //       return EditGroupPage(databaseHandler: _appRepository, groupId: groupId);
-      //     }),
       GoRoute(
           name: AppRoutes.userDetails,
           path: '/user_details',
@@ -32,18 +24,6 @@ class AppRouter {
             final userId = extras['userId'] as int;
             return UserDetailsPage(appRepository: _appRepository, userId: userId);
           }),
-      // GoRoute(
-      //     name: AppRoutes.createGroup,
-      //     path: '/create_group',
-      //     builder: (BuildContext context, GoRouterState state) {
-      //       return CreateGroupPage(databaseHandler: _appRepository);
-      //     }),
-      // GoRoute(
-      //     name: AppRoutes.createPerson,
-      //     path: '/create_person',
-      //     builder: (BuildContext context, GoRouterState state) {
-      //       return  CreatePersonPage(databaseHandler: _appRepository);
-      //     }),
     ],
   );
 
--- lib/services/router/app_routes.dart
+++ lib/services/router/app_routes.dart
@@ -1,7 +1,4 @@
 class AppRoutes {
   static const dashboard = 'dashboard';
-  static const createPerson = 'create_person';
   static const userDetails = 'user_details';
-  static const createGroup = 'create_group';
-  static const editGroup = 'edit_group';
 }
